5edcf388521fd7b2adf0c6dcb5c88b3ef37fc94a
Aaron Nguyen <aaronnguyen.vic@gmail.com>
2020-04-05 22:23:51 +1000

    Add egg groups and EV yields

--- src/components/Pokemon/PokemonDetailsGeneral.vue
+++ src/components/Pokemon/PokemonDetailsGeneral.vue
@@ -15,18 +15,55 @@
       <q-card class="col pkm-details-card" align="center" dark bordered>{{ pokemon.weight }} kg</q-card>
     </div>
     <div class="row text-overline pkm-details-title">Gender Ratio</div>
-    <div class="row">
-        <q-linear-progress class="pkm-details-gender-male" :style="getGenderStyle(pokemon.genderRatio.male, true)" size="25px" :value="100/100">
+    <div
+      class="row pkm-details-content"
+      v-if="pokemon.genderRatio.male !== 0 && pokemon.genderRatio.female !== 0"
+    >
+        <q-linear-progress class="pkm-details-gender-male" :style="getGenderStyle(pokemon.genderRatio.male, true)" size="25px" :value="1">
           <div class="absolute-full flex flex-center">
             <q-badge color="transparent" text-color="black" :label="getGenderLabel(pokemon.genderRatio.male, true)" />
           </div>
         </q-linear-progress>
-        <q-linear-progress class="pkm-details-gender-female" :style="getGenderStyle(pokemon.genderRatio.female, false)" size="25px" :value="100/100">
+        <q-linear-progress class="pkm-details-gender-female" :style="getGenderStyle(pokemon.genderRatio.female, false)" size="25px" :value="1">
           <div class="absolute-full flex flex-center">
             <q-badge color="transparent" text-color="black" :label="getGenderLabel(pokemon.genderRatio.female, false)" />
           </div>
         </q-linear-progress>
     </div>
+    <div
+      class="row pkm-details-content"
+      v-else
+    >
+      <q-linear-progress dark rounded size="25px" :value="1" color="grey">
+          <div class="absolute-full flex flex-center">
+            <q-badge color="transparent" text-color="black" :label="pokemonIsGenderless" />
+          </div>
+        </q-linear-progress>
+    </div>
+    <div class="row text-overline pkm-details-title">Egg Groups</div>
+    <div class="row pkm-details-title pkm-details-content">
+      <q-btn
+        color="white"
+        class="col bg-black pkm-details-button"
+        v-for="eggGroup in pokemon.eggGroups"
+        :key="eggGroup"
+        outline
+      >
+        {{ eggGroup }}
+      </q-btn>
+    </div>
+    <div class="row text-overline pkm-details-title">EV Yields</div>
+    <q-card
+      dark
+      bordered
+      class="pkm-details-card pkm-details-title">
+      <q-card-section
+        v-for="ev in pokemon.evYields"
+        :key="ev"
+      >
+        {{ ev }}
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
@@ -35,10 +72,12 @@ export default {
   props: ['pokemon'],
   data () {
     return {
+      pokemonIsGenderless: this.pokemon.name + ' is genderless'
     }
   },
   methods: {
     getGenderLabel (gender, isMale) {
+      console.log(this.pokemon)
       let label = ''
       if (gender !== undefined && gender > 0) {
         label = (isMale ? '♂ ' : ' ♀') + String(gender.toFixed(1)) + '%'
@@ -67,9 +106,17 @@ export default {
 </script>
 
 <style lang="scss">
+  .pkm-details-button {
+    margin: 0 5px;
+  }
+
+  .pkm-details-content {
+    margin-bottom: 8px;
+  }
+
   .pkm-details-card {
     padding: 8px;
-    margin-bottom: 8px;
+    margin: 0px 5px 8px;
   }
 
   div {
